f8fdc16003695287c06dc95d2d368c4f98c2550a
Eric Dalongeville <22616866+edalongeville@users.noreply.github.com>
2020-04-09 01:49:06 +0200

    Added AZ/Region parameters check

--- __main__.py
+++ __main__.py
@@ -1,7 +1,14 @@
 import pulumi
 import pulumi_aws as aws
 
-import storage
-import access
-import security
-import instance
+config = pulumi.Config("nextcloud")
+availabilityZone = config.require("availabilityZone")
+region = pulumi.Config("aws").require("region")
+
+if region in availabilityZone:
+    import storage
+    import access
+    import security
+    import instance
+else:
+    pulumi.log.error("Please chose an AZ from the defined Region")
